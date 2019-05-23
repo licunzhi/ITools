a8595353f669ab6721967889b21c40827ecec97f
licunzhi <2718272293@qq.com>
2019-05-23 14:52:33 +0800

    3.0JSON视图展示

--- README.md
+++ README.md
@@ -5,3 +5,5 @@
 ![版本展示](show.jpg)  
 ### 2、支持md5，base64加密以及图片转换 下载地址：[ITools-2.0.crx](https://raw.githubusercontent.com/licunzhi/ITools/master/crx/ITools-2.0.crx)    
 ![版本展示](2.0.jpg)  
+### 3、支持json数据格式化以及视图的展示下载地址：[ITools-3.0.crx](https://raw.githubusercontent.com/licunzhi/ITools/master/crx/ITools-3.0.crx)    
+![版本展示](3.0.jpg) 
--- js/json/index.js
+++ js/json/index.js
@@ -14,6 +14,14 @@ $(function () {
         }
     }
 
+    /*刷新缓存中数据*/
+    $('[id]').blur(function () {
+        $('textarea[id]').each(function (index, value) {
+            TS[$(value).attr('id')] = $(value).val();
+        });
+        localStorage.setItem('SAKURA', JSON.stringify(TS));
+    });
+
     /*导航点击事件 展示或者隐藏执行的代码块*/
     $('.jsonFormat li').click(function () {
         /*自己本省不做操作*/
@@ -27,6 +35,69 @@ $(function () {
         }
     });
 
+    /*复制命令*/
+    $('#copyJson').click(function () {
+        copyConvertResult();
+    });
+
+    /*格式化点击事件*/
+    $('#formatJson').click(function () {
+        var jsonText = $('#jsonText').val();
+        try {
+            var  parseObj = JSON.parse(jsonText);
+            $('#jsonText').val(JSON.stringify(parseObj, null , 4));
+        } catch (e) {
+            alert('请检查是否满足json格式');
+        }
+    });
+
+    /*空格进行替换的点击事件*/
+    $('#deleteSpace').click(function () {
+        var jsonText = $('#jsonText').val();
+        var replaceAll = jsonText.replace(/\s/g,"");
+        $('#jsonText').val(replaceAll);
+    });
+
+    /*删除空格之后进行转义点击事件*/
+    $('#deleteSpaceFormat').click(function () {
+        var jsonText = $('#jsonText').val();
+        var replaceAll = jsonText.replace(/\s/g, "");
+        replaceAll = replaceAll.replace(/(\")/g, "\\$1");
+        $('#jsonText').val(replaceAll);
+    });
+
+    /*去除转义点击事件*/
+    $("#deleteFormat").click(function () {
+        var jsonText = $('#jsonText').val();
+        var replaceAll = jsonText.replace(/\\/g, "");
+        $('#jsonText').val(replaceAll);
+    });
+
+    /*去除转移之后格式化*/
+    $("#deleteFormatAndFormat").click(function () {
+        var jsonText = $('#jsonText').val();
+        var replaceAll = jsonText.replace(/\\/g, "");
+        try {
+            var  parseObj = JSON.parse(replaceAll);
+            $('#jsonText').val(JSON.stringify(parseObj, null , 4));
+        } catch (e) {
+            alert('请检查是否满足json格式');
+        }
+    });
+
 });
+
+
+function copyConvertResult(){
+    var Url2=document.getElementById("jsonText");
+    var val=Url2.value;
+    if(val==''){
+        alert('没有要复制的内容！');
+        return false;
+    }
+    Url2.select(); // 选择对象
+    document.execCommand("Copy"); // 执行浏览器复制命令
+    alert("已复制结果");
+}
 
 var TS = {};
